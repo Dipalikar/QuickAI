6bc8b490f7b03e46231785de07e33ff051f63f4f
Dipalikar <dipalikajal2002@gmail.com>
2025-10-24 00:14:32 +0530

    add:blog titles

--- client/src/pages/BlogTitles.jsx
+++ client/src/pages/BlogTitles.jsx
@@ -1,9 +1,72 @@
-import React from 'react'
+import { HashIcon, Sparkles } from 'lucide-react'
+import React, { useState } from 'react'
 
 const BlogTitles = () => {
+
+
+  const category=[
+    {text:"General"},
+    {text:"Technology"},
+    {text:"Business"},
+    {text:"Health"},
+    {text:"Lifestyle"},
+    {text:"Education"},
+    {text:"Travel"},
+    {text:"Food"},
+  ]
+
+  const [selectedCategory,setselectedCategory]=useState(category[0])
+  const[input,setInput]=useState('')
+  
+    const onSubmitHandler=async(e)=>{
+      e.preventDefault();
+    }
   return (
-    <div>
-        <h1>BlogTitles</h1>
+    <div className='h-full overflow-y-scroll p-6 flex items-start  gap-4 text-slate-700'>
+      {/* left col */}
+      <form onSubmit={onSubmitHandler} className='w-full max-w-lg p-4 bg-white rounded-lg border border-gray-200'>
+        <div className='flex items-center gap-3'>
+          <Sparkles className='w-6 text-[#8e37eb]'/>
+          <h1 className='text-xl font-semibold'>AI Title Generator</h1>
+          
+        </div>
+        <p className='mt-6 text-sm font-medium'>Keyword</p>
+
+        <input type='text' placeholder='the future of artificial intelligence is...' required className='w-full p-2 px-3 mt-2 outline-none text-sm rounded-md border border-gray-300' onChange={(e)=>setInput(e.target.value)}/>
+
+        <p className='mt-4 text-sm font-medium'>Category</p>
+        
+        <div className='mt-3 grid grid-cols-2 sm:grid-cols-3 flex-wrap gap-3 md:max-w-9/11 '>
+          {category.map((item,index)=>
+          <span key={index} className={`text-xs text-center px-4 py-1 border rounded-full cursor-pointer ${selectedCategory.text===item.text ? 'bg-purple-50 text-purple-700':'text-gray-500 border-gray-300'}`} onClick={()=>setselectedCategory(item)}>
+            {item.text}
+          </span>
+        )
+          }
+        </div>
+        <br/>
+        <button className='w-full flex justify-center items-center gap-2 bg-gradient-to-r from-[#c341f6] to-[#8e37eb] text-white px-4 py-2 mt-6 text-sm rounded-lg cursor-pointer'>
+          <HashIcon className='w-5'/>
+          Generate title
+        </button>
+
+      </form>
+      {/* right col */}
+      <div className='w-full max-w-lg p-4 bg-white rounded-lg flex flex-col border border-gray-200 min-h-96 max-h-[600px]'>
+        <div className='flex items-center gap-3'>
+          <HashIcon className='w-5 h-5 text-[#8e37eb]'/>
+          <h1 className='text-xl font-semibold'> Generated titles</h1>
+        </div>
+        <div className='flex-1 flex justify-center items-center'>
+          <div className='text-sm flex flex-col items-center gap-5 text-gray-400'>
+            <HashIcon className='w-9 h-9'/>
+            <p className=''>Enter a topic and click "Generate title" to get started</p>
+
+          </div>
+
+        </div>
+      </div>
+        
     </div>
   )
 }
